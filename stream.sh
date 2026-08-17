d5c98e5a30998d5e06ee62ee78790788aa1f0b14
mhndsmhnd122-creator <mhndsmhnd122@gmail.com>
2026-08-17 21:39:49 +0300

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,10 +4,15 @@ KICK_URL="https://kick.com/id7o"
 RESTREAM_KEY="re_11725544_event1f24e3174647428d86fc1329252bbf36"
 RTMP_URL="rtmp://live.restream.io/live/$RESTREAM_KEY"
 
-echo "جاري سحب البث من Kick ومعالجة الصوت لأعلى مستوى..."
+echo "جاري سحب البث ومعالجة الصوت بثبات تام..."
 
 streamlink --stdout "$KICK_URL" best | ffmpeg -i pipe:0 \
     -af "loudnorm=I=-16:TP=-1.5:LRA=11,volume=2.0" \
     -c:v copy \
     -c:a aac -b:a 192k -ar 44100 \
-    -f flv "$RTMP_URL"
+    -f flv \
+    -rtmp_live live \
+    -reconnect_at_eof 1 \
+    -reconnect_streamed 1 \
+    -reconnect_delay_max 5 \
+    "$RTMP_URL"

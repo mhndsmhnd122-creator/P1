f7d6c5d8b8370ea901ae55454c49a9ceecead25c
mhndsmhnd122-creator <mhndsmhnd122@gmail.com>
2026-08-18 15:31:25 +0300

    Update stream.sh0

--- stream.sh
+++ stream.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# تم تغيير الرابط لقناة OSAMAH
 KICK_URL="https://kick.com/OSAMAH"
 RESTREAM_KEY="re_11725544_event1f24e3174647428d86fc1329252bbf36"
 RTMP_URL="rtmp://live.restream.io/live/$RESTREAM_KEY"
 
-echo "جاري سحب البث ومعالجة الصوت بثبات تام..."
+echo "جاري سحب البث من قناة OSAMAH ومعالجة الصوت بثبات تام..."
 
 streamlink --stdout "$KICK_URL" best | ffmpeg -i pipe:0 \
     -af "loudnorm=I=-16:TP=-1.5:LRA=11,volume=2.0" \
